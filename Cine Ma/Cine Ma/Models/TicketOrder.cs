f062c957c714e682bd21399daa11fc9fa711d63e
Ryan Lirio <ryanlirio2@gmail.com>
2025-10-20 09:00:40 -0300

    Model TicketOrder feito

--- Cine Ma/Cine Ma/Models/TicketOrder.cs
+++ Cine Ma/Cine Ma/Models/TicketOrder.cs
@@ -2,5 +2,9 @@
 {
     public class TicketOrder
     {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public float Value { get; set; }
+        public bool HalfPrice { get; set; }
     }
 }
