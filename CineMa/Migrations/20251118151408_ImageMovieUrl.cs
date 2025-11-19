5a75e400b54ef2e9fc8f9146ca10bd178324da27
Ryan Lirio <ryanlirio2@gmail.com>
2025-11-18 22:36:56 -0300

    conflitos

---
+++ CineMa/Migrations/20251118151408_ImageMovieUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineMa.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageMovieUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Movie");
+        }
+    }
+}
