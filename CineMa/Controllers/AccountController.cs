f06f2af762db99528110029bf0735353f1e7af0b
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2025-11-21 16:40:30 -0300

    feito CRUD de cliente

--- CineMa/Controllers/AccountController.cs
+++ CineMa/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Cine_Ma.Data;
 using Cine_Ma.Models;
 using Cine_Ma.Repository;
+using CineMa.Helpers;
 using CineMa.Models.ViewModel;
 
 using Microsoft.AspNetCore.Mvc;
@@ -141,6 +142,155 @@ namespace CineMa.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction("Login", "Account");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAdmin()
+        {
+            if (!AdminHelper.IsAdmin(HttpContext))
+                return RedirectToAction("Index", "Home");
+
+            var users = await _context.Clients.Include(c => c.Address).ToListAsync();
+            return View("~/Views/Admin/User/Index.cshtml", users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            if (!AdminHelper.IsAdmin(HttpContext))
+                return RedirectToAction("Index", "Home");
+
+            return View("~/Views/Admin/User/Create.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterClientViewModel vm, string Role, string NewPassword)
+        {
+            if (!AdminHelper.IsAdmin(HttpContext))
+                return RedirectToAction("Index", "Home");
+
+            if (!ModelState.IsValid)
+                return View("~/Views/Admin/User/Create.cshtml", vm);
+
+            var address = new Address
+            {
+                ZipCode = vm.ZipCode,
+                City = vm.City,
+                Road = vm.Road,
+                State = vm.State,
+                Number = vm.Number,
+                Descripton = vm.Descripton
+            };
+
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+
+            var client = new Client
+            {
+                Name = vm.Name,
+                Cpf = vm.Cpf,
+                Email = vm.Email,
+                Phone = vm.Phone,
+                Birthday = vm.Birthday,
+                RegistrationDate = DateTime.Now,
+                AddressId = address.Id,
+                Role = Role,
+                SenhaHash = BCrypt.Net.BCrypt.HashPassword(NewPassword)
+            };
+
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("IndexAdmin");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            if (!AdminHelper.IsAdmin(HttpContext))
+                return RedirectToAction("Index", "Home");
+
+            var client = await _context.Clients.Include(c => c.Address).FirstOrDefaultAsync(c => c.Id == id);
+            if (client == null) return NotFound();
+
+            var vm = new RegisterClientViewModel
+            {
+                Name = client.Name,
+                Cpf = client.Cpf,
+                Email = client.Email,
+                Phone = client.Phone,
+                Birthday = client.Birthday,
+                ZipCode = client.Address.ZipCode,
+                City = client.Address.City,
+                Road = client.Address.Road,
+                State = client.Address.State,
+                Number = client.Address.Number,
+                Descripton = client.Address.Descripton
+            };
+
+            ViewBag.Role = client.Role;
+
+            return View("~/Views/Admin/User/Update.cshtml", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, RegisterClientViewModel vm, string Role, string NewPassword)
+        {
+            if (!AdminHelper.IsAdmin(HttpContext))
+                return RedirectToAction("Index", "Home");
+
+            ModelState.Remove("Senha");
+            ModelState.Remove("NewPassword");
+
+            if (!ModelState.IsValid)
+                return View("~/Views/Admin/User/Update.cshtml", vm);
+
+            var client = await _context.Clients.Include(c => c.Address).FirstOrDefaultAsync(c => c.Id == id);
+            if (client == null) return NotFound();
+
+            client.Name = vm.Name;
+            client.Cpf = vm.Cpf;
+            client.Email = vm.Email;
+            client.Phone = vm.Phone;
+            client.Birthday = vm.Birthday;
+            client.Role = Role;
+
+            client.Address.ZipCode = vm.ZipCode;
+            client.Address.City = vm.City;
+            client.Address.Road = vm.Road;
+            client.Address.State = vm.State;
+            client.Address.Number = vm.Number;
+            client.Address.Descripton = vm.Descripton;
+
+            if (!string.IsNullOrWhiteSpace(NewPassword))
+                client.SenhaHash = BCrypt.Net.BCrypt.HashPassword(NewPassword);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("IndexAdmin");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!AdminHelper.IsAdmin(HttpContext))
+                return RedirectToAction("Index", "Home");
+
+            var client = await _context.Clients.Include(c => c.Address).FirstOrDefaultAsync(c => c.Id == id);
+            if (client == null) return NotFound();
+
+            var address = client.Address;
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            if (address != null)
+            {
+                _context.Addresses.Remove(address);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("IndexAdmin");
         }
     }
 }
--- CineMa/Models/ViewModel/RegisterClientViewModel.cs
+++ CineMa/Models/ViewModel/RegisterClientViewModel.cs
@@ -3,6 +3,7 @@
     public class RegisterClientViewModel
     {
         // CLIENTE
+        public int? Id { get; set; }
         public string Name { get; set; }
         public string Cpf { get; set; }
         public string Email { get; set; }
